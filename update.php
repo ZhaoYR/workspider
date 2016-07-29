0c160dd9942d074fa43c562770c9409deb1c248f
zhaoyirong <yirong@iscas.ac.cn>
2016-07-29 14:49:15 +0800

    编辑新闻页面

--- update.php
+++ update.php
@@ -13,12 +13,12 @@
       // 提取控件  
       var checkbox = thisform.elements[i];  
       // 检查是否是指定的控件  
-      if (checkbox.name === "groupCheckbox" && checkbox.type === "checkbox" && checkbox.checked === false)  
+      if (checkbox.name === "groupCheckbox[]" && checkbox.type === "checkbox" && checkbox.checked === false)  
       {  
         // 正选  
         checkbox.checked = true;  
       }  
-      else if (checkbox.name === "groupCheckbox" && checkbox.type === "checkbox" && checkbox.checked === true)  
+      else if (checkbox.name === "groupCheckbox[]" && checkbox.type === "checkbox" && checkbox.checked === true)  
       {  
         // 反选  
         checkbox.checked = false;  
