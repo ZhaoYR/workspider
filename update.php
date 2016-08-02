953d4fa2bf1169803d32ec6378f9ef81418bc39a
zhaoyirong <yirong@iscas.ac.cn>
2016-08-02 11:31:15 +0800

    更新了编辑页面的前端展示

--- update.php
+++ update.php
@@ -41,6 +41,67 @@
 </head>
 
 <body>
+
+<table width="300" height="100" border="0" align="center" cellpadding="0" cellspacing="0" >
+ <?php
+
+	$dbcolarray = array('username', 'password');  
+	$conn = new mysqli ("localhost","root","","spider");
+	mysqli_query($conn,"set names utf8");	//对数据库中编码格式进行转换，避免出现中文乱码的问题
+	if (mysqli_connect_errno())
+	{
+		echo 'Error : Could not connect to database . ';
+		exit;
+	}
+
+	  $select=mysqli_query($conn,"select * from user where id = '1' ");
+	  $array=mysqli_fetch_array($select);
+
+?>
+
+    <tr>
+    	<div align="center">期刊设置</div>
+    </tr>
+
+    <tr>
+      <td align="center" valign="middle" >      
+        <form action="set_ok.php" method="post" enctype="multipart/form-data" name="form2" id="form2">
+          <table cellpadding="0" cellspacing="0">
+            <tr>
+              <td>
+                <div align="left" width="30">期数：</div>
+              </td>
+              <td>
+                <div align="left">
+                  <input name="username" type="text" id="username" value="<?php echo $array['username'];?>" size="50" /></div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div align="left" width="30">时间：</div>
+              </td>
+              <td>
+                <div align="left">
+                  <input name="password" type="date" id="password" value="<?php $array['password']=substr($array['password'], 0, 10); echo $array['password'];?>" size="50" /></div>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <div align="center">
+                  <input type="submit" name="Submit" value="提交" />
+                  <input type="hidden" name="id" value="<?php echo $array['id'];?>"/>
+                  <!--隐藏域--> </div>
+              </td>
+            </tr>
+          </table>
+      </td>
+    </tr> 
+	</form>
+
+
+</table>
+
+
 	<?php
 include("conn/conn.php");
 $descript = mysqli_query($conn,"update news set origin = content ");
@@ -156,7 +217,7 @@ $descript = mysqli_query($conn,"update news set origin = content ");
 						<td colspan="8" align="center">
 					    	<input name="" type="button" class="input_hide" onClick="SelectAll(this.form);return false;" value="全选">
 					    	<input name="" type="button" class="input_hide" onClick="CheckSelect(this.form);return false;" value="反选">
-					        <input type="submit" name="submit" value="选中数据" /> 
+					        <input type="submit" name="submit" value="选中数据生成页面" /> 
                   			<input type="hidden" name="id" value=""/>
 					     </form>
 					   </td>				
@@ -189,76 +250,10 @@ $descript = mysqli_query($conn,"update news set origin = content ");
 					</tr>
 
 					
-				</table>
-			</form>
+				</table><!-- 
+			</form> -->
 		</td>
 	</tr>
-</table>
-
-<table width="500" height="150" border="0" align="center" cellpadding="0" cellspacing="0" >
- <?php
-
-	$dbcolarray = array('username', 'password');  
-	$conn = new mysqli ("localhost","root","","spider");
-	mysqli_query($conn,"set names utf8");	//对数据库中编码格式进行转换，避免出现中文乱码的问题
-	if (mysqli_connect_errno())
-	{
-		echo 'Error : Could not connect to database . ';
-		exit;
-	}
-
-	  $select=mysqli_query($conn,"select * from user where id = '1' ");
-	  $array=mysqli_fetch_array($select);
-
-?>
-
-    <tr>
-    	<div align="center">期刊设置</div>
-    </tr>
-
-    <tr>
-      <td align="center" valign="middle" >      
-        <form action="set_ok.php" method="post" enctype="multipart/form-data" name="form2" id="form2">
-          <table cellpadding="0" cellspacing="0">
-            <tr>
-              <td>
-                <div align="left" width="30">期数：</div>
-              </td>
-              <td>
-                <div align="left">
-                  <input name="username" type="text" id="username" value="<?php echo $array['username'];?>" size="50" /></div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div align="left" width="30">时间：</div>
-              </td>
-              <td>
-                <div align="left">
-                  <input name="password" type="date" id="password" value="<?php $array['password']=substr($array['password'], 0, 10); echo $array['password'];?>" size="50" /></div>
-              </td>
-            </tr>
-            <tr>
-              <td colspan="2">
-                <div align="center">
-                  <input type="submit" name="Submit" value="提交" />
-                  <input type="hidden" name="id" value="<?php echo $array['id'];?>"/>
-                  <!--隐藏域--> </div>
-              </td>
-            </tr>
-          </table>
-      </td>
-    </tr> 
-	</form>
-
-	<tr>
-		<td align="center">
-			<p>
-				<a href="result.php" target="_blank"><button type = "buton">生成新闻页面</button></a>
-			</p>
-		</td>
-	</tr>
-
 </table>
 
 </body>
