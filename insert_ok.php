81cecc992abbe29248cd51be361f0e4caafa36bb
zhaoyirong <yirong@iscas.ac.cn>
2016-08-02 11:30:26 +0800

    插入成功，增加了正文项

--- insert_ok.php
+++ insert_ok.php
@@ -1,7 +1,7 @@
 <?php 
 header("content-type:text/html;charset=utf-8");		//设置文件编码
 include("conn/conn.php");							//包含数据库连接文件
-if (!$_POST['title']||!$_POST['url']||!$_POST['time']||!$_POST['content']||!$_POST['ip']) 
+if (!$_POST['title']||!$_POST['url']||!$_POST['time']||!$_POST['content']||!$_POST['ip']||!$_POST['origin']) 
 {
 	echo "请输入一条完整的新闻。<br>";
 	echo "<a href='insert.php'>	<button type = 'button'>返回</button></a>";
@@ -14,7 +14,8 @@ else if(isset($_POST['Submit']) and $_POST['Submit']=="提交")
 	$ip=$_POST['ip'];
 	$time=$_POST['time'];
 	$content=$_POST['content'];
-	$insert=mysqli_query($conn,"insert into news(title,url,ip,time,content) values('$title','$url','$ip','$time','$content')");		//执行添加操作
+	$origin=$_POST['origin'];
+	$insert=mysqli_query($conn,"insert into news(title,url,ip,time,content，origin) values('$title','$url','$ip','$time','$content','$origin')");		//执行添加操作
 	echo mysqli_error($conn);
 	if($insert)
 	{							//判断添加语句是否执行成功
